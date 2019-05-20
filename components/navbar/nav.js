715b95e2e52c342522d2797a2fec3566e39ca3d1
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-20 16:56:24 -0400

    UI Nav Made nicer

--- components/navbar/nav.js
+++ components/navbar/nav.js
@@ -1,9 +1,11 @@
 let navbase = document.querySelector("body > div");
 let navOpen = document.querySelector(".home-icon");
 let drawer = document.querySelector(".drawer");
+let darken = document.querySelector(".lighten");
 
 opendrawer = () => {
   drawer.className =
     drawer.className === "drawerOpen" ? "drawer" : "drawerOpen";
+  darken.className = darken.className === "darken" ? "lighten" : "darken";
 };
 navOpen.addEventListener("click", this.opendrawer);
